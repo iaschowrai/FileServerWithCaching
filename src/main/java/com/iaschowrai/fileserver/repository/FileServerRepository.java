8004f131c2ec8c82977857b7a3387fe79e8c8180
Irshad Ahmed <iaschowrai@gmail.com>
2024-03-15 15:18:13 -0400

    FileServer Service Impl upload method created

--- src/main/java/com/iaschowrai/fileserver/repository/FileServerRepository.java
+++ src/main/java/com/iaschowrai/fileserver/repository/FileServerRepository.java
@@ -2,7 +2,9 @@ package com.iaschowrai.fileserver.repository;
 
 import com.iaschowrai.fileserver.model.FileServer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FileServerRepository extends JpaRepository<FileServer, Long> {
 
 }
--- src/main/java/com/iaschowrai/fileserver/service/FileServerServiceImpl.java
+++ src/main/java/com/iaschowrai/fileserver/service/FileServerServiceImpl.java
@@ -2,14 +2,80 @@ package com.iaschowrai.fileserver.service;
 
 import com.iaschowrai.fileserver.dto.FileUploadResponse;
 import com.iaschowrai.fileserver.model.FileServer;
+import com.iaschowrai.fileserver.repository.FileServerRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class FileServerServiceImpl implements FileServerService{
 
+    @Value("${file.upload.path}")
+    public String uploadPath;
+
+
+    private final FileServerRepository fileServerRepository;
 
     @Override
     public FileUploadResponse uploadFile(MultipartFile file) {
-        return null;
+
+        try{
+            if(!Files.exists(Path.of(uploadPath))){
+                // create a upload folder
+                Files.createDirectories(Path.of(uploadPath));
+            }
+
+            // Original file
+            //image.png - > .png
+
+            var originalFilename = file.getOriginalFilename();
+
+            // File extension
+            var fileExtension = originalFilename.substring(originalFilename.lastIndexOf("."));
+
+            // upload file name
+            var uploadFilename = UUID.randomUUID() + fileExtension;
+
+            // create meta-data object
+
+            var fileMetadata = FileServer.builder()
+                    .fileName(originalFilename)
+                    .fileExtension(fileExtension)
+                    .uploadFileName(uploadFilename)
+                    .build();
+
+            // save file meta-data
+
+            fileServerRepository.save(fileMetadata);
+
+            //save file in local storage
+            // .uploads/uploadFilename.extension
+            var targetPath = uploadPath +"/" +uploadFilename;
+
+            // copy the file to target location
+            Files.copy(file.getInputStream(), Path.of(targetPath) , StandardCopyOption.REPLACE_EXISTING);
+
+            // Return the file upload response
+
+            return FileUploadResponse.builder()
+                    .fileName(uploadFilename)
+                    .downloadUrl("/api/files/download/" + uploadFilename)
+                    .build();
+
+
+        }catch (Exception e){
+            throw new EntityNotFoundException();
+        }
     }
 
     @Override
